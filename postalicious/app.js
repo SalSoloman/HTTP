8a7e0cb560f51838d8255c6e7052d606747dcc1a
John Ware <johnware@Johns-MacBook-Air.local>
2017-04-12 13:47:47 -0700

    HTML and CSS

---
+++ postalicious/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const http = require('http').createServer(app)
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.get('/', function(request, response){
+  response.send("Are you working?")
+})
+
+
+http.listen(3001, function(request, response){
+  console.log("Listening on Port 3001");
+})
+module.export = app
